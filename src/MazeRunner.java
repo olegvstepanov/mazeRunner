28295895b384c3ec6a5da961005ec00f3db3283a
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-10-09 13:25:37 +0200

    added intro, userMove, movesMessage methods

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -14,6 +14,17 @@ public class MazeRunner {
         System.out.println("----------------------------------------");
     }
 
+    public static String userMove(){
+        Scanner moveInput = new Scanner(System.in);
+        System.out.println("Where would you like to move? (R, L, U, D)");
+        String move = moveInput.next().toUpperCase();
+        if(move.equals("R") || move.equals("L") || move.equals("U") || move.equals("D")){
+            return move;
+        } else {
+            return userMove();
+        }
+    }
+
     public static String userMover(String move){
 
         if (myMap.canIMoveRight()&&move.equals("R")){
@@ -35,6 +46,19 @@ public class MazeRunner {
     }
 
     public static void movesMessage(int moves){
+
+        if(moves == 50){
+            System.out.println("Warning, you have 50 moves to complete the maze");
+        }
+        if(moves == 75){
+            System.out.println("Warning, you have made 75 moves, only 25 moves left");
+        }
+        if(moves == 90){
+            System.out.println("Warning, you have made 90 moves, only 10 moves left");
+        }
+        if(moves == 100){
+            System.out.println("Oh no !!! you have no more moves left and now the maze is closed");
+        }
 
     }
 
